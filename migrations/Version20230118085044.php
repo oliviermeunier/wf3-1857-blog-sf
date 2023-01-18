1dc4d402146cfe7474e4e01a9b45118eed5b578f
oliviermeunier <oli.meunier@gmail.com>
2023-01-18 09:52:05 +0100

    Blog - CH5 - Change DB name

--- migrations/Version20230118085044.php
+++ migrations/Version20230118085044.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230112130012 extends AbstractMigration
+final class Version20230118085044 extends AbstractMigration
 {
     public function getDescription(): string
     {
